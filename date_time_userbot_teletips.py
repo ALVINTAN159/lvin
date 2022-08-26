3f420dc718172353545f776931c545b2742bb153
ALVINTAN159 <112139758+ALVINTAN159@users.noreply.github.com>
2022-08-26 11:01:17 +0800

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -44,6 +44,7 @@ async def main_teletips():
                 await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = Date_Time_Userbot_teletips.get_chat_photos("me")
+                try:
                 await Date_Time_Userbot_teletips.DeletePhotosRequest(photos[8].file_id)
                 except Exception:
                     pass        
