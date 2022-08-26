dcf7e617acaf2575bcea960dad57cd7d38a52c07
ALVINTAN159 <112139758+ALVINTAN159@users.noreply.github.com>
2022-08-26 10:38:35 +0800

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -42,8 +42,7 @@ async def main_teletips():
                 await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = Date_Time_Userbot_teletips.get_chat_photos("me")
-                try:
-                    await Date_Time_Userbot_teletips.delete_profile_photos(photos[8].file_id)
+                await Date_Time_Userbot_teletips.delete_profile_photos(photos[8].file_id)
                 except Exception:
                     pass        
                 print("Profile Updated!")
