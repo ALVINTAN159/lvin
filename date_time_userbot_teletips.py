1578affe0c4ff49e3b3049ce7edd8a5b6ad14e27
ALVINTAN159 <112139758+ALVINTAN159@users.noreply.github.com>
2022-10-09 19:04:39 +0800

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -1,53 +1,11 @@
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
-from lists_teletips.quotes_teletips import *
-from lists_teletips.emojis_teletips import *
-from PIL import Image, ImageDraw, ImageFont
-import datetime
-import pytz
-import asyncio
-import random
-import os
+from telethon import TelegramClient, sync
+from telethon.tl.functions.photos import DeletePhotosRequest
 
-Date_Time_Userbot_teletips=Client(
-    name = "date_time_userbot_teletips",
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
-    session_string = os.environ["SESSION_STRING"]
-)
+api_id = "14771029"
+api_hash = "aadfc65b33278f6548104180f7e7b4ad"
 
-Time_Zone = os.environ["TIME_ZONE"]
 
-async def main_teletips():
-    try:
-        while True:
-            if Date_Time_Userbot_teletips.is_connected:
-                Quotes_teletips = random.choice(quotes_teletips)
-                Emojis_teletips = random.choice(emojis_teletips)
-                TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
-                Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
-                Date_teletips = TimeZone_teletips.strftime("%b %d") 
-                Image_teletips = Image.open("image.jpg")
-                Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
-                Image_text_teletips = f"{Time_teletips}"
-                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
-                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
-                Image_teletips.save("Image_final_teletips.jpg")
-                await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| ⏰ {Time_teletips} | 📅 {Date_teletips}")
-                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
-                me = await Date_Time_Userbot_teletips.get_me()
-                photos = Date_Time_Userbot_teletips.get_chat_photos("me")
-                Date_Time_Userbot_teletips.delete_profile_photo(photos[1].file_id)
-                try:
-                    print("DELETED PHOTO")
-                except Exception:
-                    pass        
-                print("Profile Updated!")
-            await asyncio.sleep(60)     
-    except FloodWait as e:
-        await asyncio.sleep(e.x)         
+client = TelegramClient("BQBIhvSWaaG7aMRxFUVPYOGzEoas7aI_fyOFg2VvUXj6-dPKhwBLNOrrI8nqJBgwJ0PAxCs2FQ-gAKO-DQwgVzt1ogvvNswzESF3FWPdfU00IffYfoTgc2u8_lN_uemCA4lnAiIBaeOeVsUf9BpUE7N2V9k6tnt137_X0SYF02k1zOIhBu9jnWRwUvbkKzT6EZlt4wu8Z9VO_x5gxzsRFMiPf3M7kub5KkWXzUWP7V_9QcXTnb2a0KfQSEjLB6edMmw0C57aR2jkOCz7U_FSeQHpbiqkcVMKgz-06qDVn_30dz6jaj0OpwGXJPYGHGtPF1-YC8SBiWiQ3k0YF8D46kUkfDvGSAA",14771029, BQBIhvSWaaG7aMRxFUVPYOGzEoas7aI_fyOFg2VvUXj6-dPKhwBLNOrrI8nqJBgwJ0PAxCs2FQ-gAKO-DQwgVzt1ogvvNswzESF3FWPdfU00IffYfoTgc2u8_lN_uemCA4lnAiIBaeOeVsUf9BpUE7N2V9k6tnt137_X0SYF02k1zOIhBu9jnWRwUvbkKzT6EZlt4wu8Z9VO_x5gxzsRFMiPf3M7kub5KkWXzUWP7V_9QcXTnb2a0KfQSEjLB6edMmw0C57aR2jkOCz7U_FSeQHpbiqkcVMKgz-06qDVn_30dz6jaj0OpwGXJPYGHGtPF1-YC8SBiWiQ3k0YF8D46kUkfDvGSAA)
+client.start()
 
-print("DATE TIME USERBOT IS ALIVE!")
-asyncio.ensure_future(main_teletips())
-Date_Time_Userbot_teletips.run()
-
+client(DeletePhotosRequest(client.get_profile_photos('me')))
